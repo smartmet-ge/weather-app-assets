c41369726fbaea0d0bf6b9f268ba47b0fb69a183
Pekka Keränen <pekka.keranen@geosaaga.fi>
2026-03-19 15:30:58 +0200

    Added i18n for translations

---
+++ i18n/index.ts
@@ -0,0 +1,63 @@
+import i18n, { LanguageDetectorAsyncModule } from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import * as RNLocalize from "react-native-localize";
+
+import { getItem, LOCALE } from '@utils/async_storage';
+import en from './en.json';
+import ka from './ka.json';
+
+import defaultConfig from 'defaultConfig';
+
+const { languages } = defaultConfig.settings;
+const languageResources = { en, ka };
+
+const getDeviceLanguage = () => {
+  const locales = RNLocalize.getLocales();
+
+  if (locales.length > 0) {
+    return locales[0].languageTag; // For example "fi-FI"
+  }
+
+  return languages[0] || 'en';
+};
+
+
+let systemLng = getDeviceLanguage().split('-')[0];
+
+if (Object.keys(languageResources).indexOf(systemLng) < 0) {
+  systemLng = "";
+}
+
+const languageDetector = <LanguageDetectorAsyncModule>{
+  init: () => {},
+  type: 'languageDetector',
+  async: true,
+  detect: async (callback: any) => {
+    const savedDataJSON = await getItem(LOCALE);
+    const lng = savedDataJSON || null;
+    const selectLanguage = lng || systemLng || languages[0] || 'en';
+    callback(selectLanguage);
+  },
+  cacheUserLanguage: () => {},
+};
+
+i18n
+  .use(languageDetector)
+  .use(initReactI18next)
+  .init({
+    compatibilityJSON: 'v3',
+    fallbackLng: languages[0] || 'en',
+    resources: languageResources,
+    ns: ['navigation'],
+    defaultNS: 'navigation',
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    react: {
+      bindI18n: 'languageChanged',
+      bindI18nStore: false,
+    },
+  });
+
+export default i18n;
