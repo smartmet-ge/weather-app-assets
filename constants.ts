02f28009f0ca9189ceefe9f47d65371099bdad39
Pekka Keränen <pekka.keranen@geosaaga.fi>
2026-06-09 16:45:24 +0300

    Updated font names to constants.ts

--- constants.ts
+++ constants.ts
@@ -2,8 +2,8 @@
 
 export const IOS_APP_GROUP = 'group.fi.fmi.mobileweather.settings'; // Should be empty if widgets are not used
 
-export const LIGHT_FONT = 'Firago-Light';
-export const REGULAR_FONT = 'Firago-Regular';
-export const MEDIUM_FONT = 'Firago-Medium';
-export const BOLD_FONT = 'Firago-Bold';
-export const SEMIBOLD_FONT = 'Firago-SemiBold';
+export const LIGHT_FONT = 'FiraGO-Light';
+export const REGULAR_FONT = 'FiraGO-Regular';
+export const MEDIUM_FONT = 'FiraGO-Medium';
+export const BOLD_FONT = 'FiraGO-Bold';
+export const SEMIBOLD_FONT = 'FiraGO-SemiBold';
