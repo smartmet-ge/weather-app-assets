8bc8854a32a653ae09195072d00f79e90499c4ae
Pekka Keränen <pekka.keranen@geosaaga.fi>
2026-06-09 16:42:13 +0300

    Removed Roboto fonts, added FiraGO fonts

--- constants.ts
+++ constants.ts
@@ -1,3 +1,9 @@
 // Settings that can't be configured in DynamicConfig, for example for non React files
 
 export const IOS_APP_GROUP = 'group.fi.fmi.mobileweather.settings'; // Should be empty if widgets are not used
+
+export const LIGHT_FONT = 'Firago-Light';
+export const REGULAR_FONT = 'Firago-Regular';
+export const MEDIUM_FONT = 'Firago-Medium';
+export const BOLD_FONT = 'Firago-Bold';
+export const SEMIBOLD_FONT = 'Firago-SemiBold';
